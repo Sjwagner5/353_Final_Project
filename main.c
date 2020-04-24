a3a2b78834316febed830d030593399af59cc080
Zach <zachkbc@gmail.com>
2020-04-24 16:13:33 -0500

    UART Pause/Running

--- main.c
+++ main.c
@@ -1,23 +1,23 @@
 // Copyright (c) 2015-19, Joe Krachey
 // All rights reserved.
 //
-// Redistribution and use in source or binary form, with or without modification, 
+// Redistribution and use in source or binary form, with or without modification,
 // are permitted provided that the following conditions are met:
 //
-// 1. Redistributions in source form must reproduce the above copyright 
-//    notice, this list of conditions and the following disclaimer in 
+// 1. Redistributions in source form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
 //    the documentation and/or other materials provided with the distribution.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 // EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "main.h"
@@ -40,16 +40,36 @@ void EnableInterrupts(void)
   }
 }
 
+
 void game_main(void) {
+	char lastKey;
 	bool gameOver = false;
+	
+	printf("Running...\n");
+	
 	while (!gameOver) {
-		gp_timer_wait(TIMER1_BASE, 5000000);
+		
+		// UART0: Pause and resume when space bar is hit. 
+		lastKey = UART0_Rx_Buffer.array[(UART0_Rx_Buffer.produce_count - 1) % UART_BUFFER_SIZE];
+		if(lastKey == ' ') {
+			printf("Paused.\n");
+			pc_buffer_add(&UART0_Rx_Buffer, '`');
+			lastKey = UART0_Rx_Buffer.array[(UART0_Rx_Buffer.produce_count - 1) % UART_BUFFER_SIZE];
+			while(lastKey != ' '){
+				lastKey = UART0_Rx_Buffer.array[(UART0_Rx_Buffer.produce_count - 1) % UART_BUFFER_SIZE];
+			}
+			pc_buffer_add(&UART0_Rx_Buffer, '`');
+			printf("Running...\n");
+		}
+		
+		
+		
 	}
 }
 
 //*****************************************************************************
 //*****************************************************************************
-int 
+int
 main(void)
 {
 		init_hardware();
